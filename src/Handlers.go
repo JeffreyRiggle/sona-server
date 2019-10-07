18d5c82cf4acbb2094533d4a8d311ba493572453
Jeff <JeffreyRiggle@gmail.com>
2019-10-07 06:32:29 -0400

    refactor user handlers and validate more requests

--- src/Handlers.go
+++ src/Handlers.go
@@ -13,7 +13,6 @@ import (
 var fileManager FileManager
 var incidentManager IncidentManager
 var hookManager HookManager
-var userManager UserManager
 
 // HandleCreateIncident handles the create incident web request.
 func HandleCreateIncident(w http.ResponseWriter, r *http.Request) {
@@ -126,6 +125,13 @@ func HandleGetAttachments(w http.ResponseWriter, r *http.Request) {
 	logManager.LogPrintln("Getting attachments")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	vars := mux.Vars(r)
 
 	incidentId, err := strconv.Atoi(vars["incidentId"])
@@ -159,6 +165,13 @@ func HandleGetAttachments(w http.ResponseWriter, r *http.Request) {
 func HandleUploadAttachment(w http.ResponseWriter, r *http.Request) {
 	logManager.LogPrintln("Got upload attachment request")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
 
 	vars := mux.Vars(r)
 	incidentId, err := strconv.Atoi(vars["incidentId"])
@@ -210,6 +223,13 @@ func HandleDownloadAttachment(w http.ResponseWriter, r *http.Request) {
 	logManager.LogPrintln("Got download request.")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	vars := mux.Vars(r)
 
 	incidentId := vars["incidentId"]
@@ -240,6 +260,13 @@ func HandleDownloadAttachment(w http.ResponseWriter, r *http.Request) {
 
 func HandleRemoveAttachment(w http.ResponseWriter, r *http.Request) {
 	logManager.LogPrintln("Got remove attachment request.")
+
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
 
 	vars := mux.Vars(r)
 
@@ -303,6 +330,13 @@ func HandleGetIncident(w http.ResponseWriter, r *http.Request) {
 	logManager.LogPrintln("Got incident state request")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	vars := mux.Vars(r)
 
 	incidentId, err := strconv.Atoi(vars["incidentId"])
@@ -333,6 +367,13 @@ func HandleGetIncidents(w http.ResponseWriter, r *http.Request) {
 	logManager.LogPrintln("Got incidents request")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
 
 	filter, passed := convertFilter(r)
 
@@ -380,236 +421,5 @@ func convertFilter(r *http.Request) (*FilterRequest, bool) {
 	return filter, true
 }
 
-func HandleCreateUser(w http.ResponseWriter, r *http.Request) {
-	logManager.LogPrintln("Got Create User request")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	addUser, pass := convertAddUser(r.Body)
-	if !pass {
-		logManager.LogPrintf("Bad request for create user %v", addUser)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	passed, user := userManager.AddUser(&addUser)
-	if !passed {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	logManager.LogPrintf("Created user %v\n", user.Id)
-	go hookManager.CallAddedUserHooks(user)
-
-	data, err := json.Marshal(user)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(data)
-}
-
-func convertAddUser(body io.ReadCloser) (AddUser, bool) {
-	decoder := json.NewDecoder(body)
-
-	var user AddUser
-	err := decoder.Decode(&user)
-
-	if err != nil {
-		logManager.LogPrintf("Got error when attempting to decode body %v", err)
-		return user, false
-	}
-
-	if len(user.EmailAddress) == 0 || len(user.UserName) == 0  || len(user.Password) == 0 {
-		return user, false
-	}
-
-	return user, true
-}
-
-func HandleUpdateUser(w http.ResponseWriter, r *http.Request) {
-	logManager.LogPrint("Got User Update")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/json")
-
-	vars := mux.Vars(r)
-
-	userId, err := strconv.Atoi(vars["userId"])
-
-	if err != nil {
-		logManager.LogPrintf("Error converting userId %v", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	update, passed := convertUpdateUser(r.Body)
-
-	if !passed {
-		logManager.LogPrintf("Invalid update for %v\n", userId)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if userManager.UpdateUser(userId, &update) {
-		// go hookManager.CallUpdatedHooks(incidentId, update)
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
-	logManager.LogPrintf("User %v not found\n", userId)
-	w.WriteHeader(http.StatusNotFound)
-}
-
-func convertUpdateUser(body io.ReadCloser) (User, bool) {
-	decoder := json.NewDecoder(body)
-
-	var update User
-	err := decoder.Decode(&update)
-
-	if err != nil {
-		return update, false
-	}
-
-	return update, true
-}
-
-func HandleGetUser(w http.ResponseWriter, r *http.Request) {
-	logManager.LogPrintln("Got user state request")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	vars := mux.Vars(r)
-
-	userId, err := strconv.Atoi(vars["userId"])
-	if err != nil {
-		logManager.LogPrintf("Error converting userId %v", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if val, ok := userManager.GetUser(userId); ok {
-		logManager.LogPrintf("Got State request for %v.", userId)
-		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-		w.WriteHeader(http.StatusOK)
-
-		if err := json.NewEncoder(w).Encode(val); err != nil {
-			panic(err)
-		}
-
-		return
-	}
-
-	logManager.LogPrintf("User %v not found\n", userId)
-	w.WriteHeader(http.StatusNotFound)
-}
-
-func HandleDeleteUser(w http.ResponseWriter, r *http.Request) {
-	logManager.LogPrintln("Got user delete request")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	vars := mux.Vars(r)
-
-	userId, err := strconv.Atoi(vars["userId"])
-
-	if err != nil {
-		logManager.LogPrintf("Error converting userId %v", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if userManager.RemoveUser(userId) {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-
-	w.WriteHeader(http.StatusInternalServerError)
-}
-
-func HandleChangePassword(w http.ResponseWriter, r *http.Request) {
-	logManager.LogPrintln("Got user password change request.")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	vars := mux.Vars(r)
-
-	userId, err := strconv.Atoi(vars["userId"])
-
-	if err != nil {
-		logManager.LogPrintf("Error converting userId %v", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	decoder := json.NewDecoder(r.Body)
-
-	var req PasswordChangeRequest
-	err2 := decoder.Decode(&req)
-
-	if err2 != nil {
-		logManager.LogPrint("Invalid password change request")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	user, found := userManager.GetUser(userId)
-
-	if !found {
-		logManager.LogPrintf("Error finding userId %v", err)
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	auth, _ := user.Authenticate(req.OldPassword)
-
-	if !auth {
-		logManager.LogPrintf("Failed to authenticate %v", userId)
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-
-	user.SetPassword(req.NewPassword)
-	w.WriteHeader(http.StatusOK)
-}
-
-func HandleAuthentication(w http.ResponseWriter, r *http.Request) {
-	logManager.LogPrint("Got User Authentication")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/json")
-
-	decoder := json.NewDecoder(r.Body)
-
-	var req UserPassword
-	err := decoder.Decode(&req)
-
-	if err != nil {
-		logManager.LogPrint("Invalid password request")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	user, found := userManager.GetUser(req.Id)
-
-	if !found {
-		logManager.LogPrintf("Unable to find user %v", req.Id)
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	auth, token := user.Authenticate(req.Password)
-
-	if !auth {
-		logManager.LogPrintf("Failed to authenticate %v", req.Id)
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-
-	if err := json.NewEncoder(w).Encode(token); err != nil {
-		panic(err)
-	}
-}
-
-// TODO finish handlers delete, get, authorize
 // TODO consider refactoring Handlers.go into multiple files.
 // TODO web hooks for update and delete user?
---
+++ src/user_handlers.go
@@ -0,0 +1,263 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+var userManager UserManager
+
+func HandleCreateUser(w http.ResponseWriter, r *http.Request) {
+	logManager.LogPrintln("Got Create User request")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	addUser, pass := convertAddUser(r.Body)
+	if !pass {
+		logManager.LogPrintf("Bad request for create user %v", addUser)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	passed, user := userManager.AddUser(&addUser)
+	if !passed {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	logManager.LogPrintf("Created user %v\n", user.Id)
+	go hookManager.CallAddedUserHooks(user)
+
+	data, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(data)
+}
+
+func convertAddUser(body io.ReadCloser) (AddUser, bool) {
+	decoder := json.NewDecoder(body)
+
+	var user AddUser
+	err := decoder.Decode(&user)
+
+	if err != nil {
+		logManager.LogPrintf("Got error when attempting to decode body %v", err)
+		return user, false
+	}
+
+	if len(user.EmailAddress) == 0 || len(user.UserName) == 0  || len(user.Password) == 0 {
+		return user, false
+	}
+
+	return user, true
+}
+
+func HandleUpdateUser(w http.ResponseWriter, r *http.Request) {
+	logManager.LogPrint("Got User Update")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["userId"])
+
+	if err != nil {
+		logManager.LogPrintf("Error converting userId %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	update, passed := convertUpdateUser(r.Body)
+
+	if !passed {
+		logManager.LogPrintf("Invalid update for %v\n", userId)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if userManager.UpdateUser(userId, &update) {
+		// go hookManager.CallUpdatedHooks(incidentId, update)
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	logManager.LogPrintf("User %v not found\n", userId)
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func convertUpdateUser(body io.ReadCloser) (User, bool) {
+	decoder := json.NewDecoder(body)
+
+	var update User
+	err := decoder.Decode(&update)
+
+	if err != nil {
+		return update, false
+	}
+
+	return update, true
+}
+
+func HandleGetUser(w http.ResponseWriter, r *http.Request) {
+	logManager.LogPrintln("Got user state request")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["userId"])
+	if err != nil {
+		logManager.LogPrintf("Error converting userId %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if val, ok := userManager.GetUser(userId); ok {
+		logManager.LogPrintf("Got State request for %v.", userId)
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(val); err != nil {
+			panic(err)
+		}
+
+		return
+	}
+
+	logManager.LogPrintf("User %v not found\n", userId)
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func HandleDeleteUser(w http.ResponseWriter, r *http.Request) {
+	logManager.LogPrintln("Got user delete request")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	token := r.Header.Get("X-Sona-Token")
+	if !userManager.ValidateUser(token) {
+		logManager.LogPrintf("Invalid Token %v used", token)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["userId"])
+
+	if err != nil {
+		logManager.LogPrintf("Error converting userId %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if userManager.RemoveUser(userId) {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func HandleChangePassword(w http.ResponseWriter, r *http.Request) {
+	logManager.LogPrintln("Got user password change request.")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["userId"])
+
+	if err != nil {
+		logManager.LogPrintf("Error converting userId %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	var req PasswordChangeRequest
+	err2 := decoder.Decode(&req)
+
+	if err2 != nil {
+		logManager.LogPrint("Invalid password change request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	user, found := userManager.GetUser(userId)
+
+	if !found {
+		logManager.LogPrintf("Error finding userId %v", err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	auth, _ := user.Authenticate(req.OldPassword)
+
+	if !auth {
+		logManager.LogPrintf("Failed to authenticate %v", userId)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	user.SetPassword(req.NewPassword)
+	w.WriteHeader(http.StatusOK)
+}
+
+func HandleAuthentication(w http.ResponseWriter, r *http.Request) {
+	logManager.LogPrint("Got User Authentication")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+
+	var req UserPassword
+	err := decoder.Decode(&req)
+
+	if err != nil {
+		logManager.LogPrint("Invalid password request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	user, found := userManager.GetUser(req.Id)
+
+	if !found {
+		logManager.LogPrintf("Unable to find user %v", req.Id)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	auth, token := user.Authenticate(req.Password)
+
+	if !auth {
+		logManager.LogPrintf("Failed to authenticate %v", req.Id)
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(token); err != nil {
+		panic(err)
+	}
+}

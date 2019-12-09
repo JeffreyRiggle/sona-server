e1e5e0182c80c7fd0cf719b10beb4cf4523e071b
Jeff <JeffreyRiggle@gmail.com>
2019-12-09 06:40:12 -0500

    add additional tests

--- test/apitest.py
+++ test/apitest.py
@@ -1,7 +1,8 @@
 import requests
 from assertpy import assert_that
 
-# Initially basic test.
+auth = ''
+
 def test_create_user():
     res = requests.post('http://localhost:8080/sona/v1/users', json={
         "emailAddress": "a@b.c",
@@ -13,13 +14,44 @@ def test_create_user():
         })
     assert_that(res.status_code).is_equal_to(201)
 
-    content = res.json()
-    assert_that(content.get("emailAddress")).is_equal_to("a@b.c")
-    assert_that(content.get("userName")).is_equal_to("TestUser")
-    assert_that(content.get("firstName")).is_equal_to("Test")
-    assert_that(content.get("lastName")).is_equal_to("User")
-    assert_that(content.get("gender")).is_equal_to("F")
-    assert_that(content.get("password")).is_equal_to(None)
-    assert_that(len(content.get("permissions"))).is_equal_to(0)
+    user1 = res.json()
+    assert_that(user1.get("emailAddress")).is_equal_to("a@b.c")
+    assert_that(user1.get("userName")).is_equal_to("TestUser")
+    assert_that(user1.get("firstName")).is_equal_to("Test")
+    assert_that(user1.get("lastName")).is_equal_to("User")
+    assert_that(user1.get("gender")).is_equal_to("F")
+    assert_that(user1.get("password")).is_equal_to(None)
+    assert_that(len(user1.get("permissions"))).is_equal_to(0)
+    assert_that(user1.get("id")).is_equal_to(1)
+
+def test_change_user_without_auth():
+    res = requests.put('http://localhost:8080/sona/v1/users/1', json={
+        "gender": "M",
+        })
+    assert_that(res.status_code).is_equal_to(403)
+
+def test_auth_user():
+    global auth
+    res = requests.post('http://localhost:8080/sona/v1/authenticate', json={
+        "id": 1,
+        "password": "changeme"
+        })
+    assert_that(res.status_code).is_equal_to(200)
+    auth = res.json().get("token")
+    assert_that(len(auth)).is_greater_than(1)
+
+def test_update_user():
+    global auth
+    res = requests.put('http://localhost:8080/sona/v1/users/1', headers={"X-Sona-Token": auth}, json={
+        "gender": "M",
+        })
+    assert_that(res.status_code).is_equal_to(200)
 
 test_create_user()
+test_change_user_without_auth()
+test_auth_user()
+test_update_user()
+# TODO update permissions
+# TODO get other user
+# TODO change password
+# TODO test delete user

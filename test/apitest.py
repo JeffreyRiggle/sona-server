0c46f2ef789fb5bb7f6f913733ed2c221b9d6ec6
Jeff <JeffreyRiggle@gmail.com>
2019-12-08 19:10:16 -0500

    make test better

--- test/apitest.py
+++ test/apitest.py
@@ -3,7 +3,7 @@ from assertpy import assert_that
 
 # Initially basic test.
 def test_create_user():
-    return requests.post('http://localhost:8080/sona/v1/users', json={
+    res = requests.post('http://localhost:8080/sona/v1/users', json={
         "emailAddress": "a@b.c",
         "userName": "TestUser",
         "firstName": "Test",
@@ -11,6 +11,15 @@ def test_create_user():
         "gender": "F",
         "password": "changeme"
         })
+    assert_that(res.status_code).is_equal_to(201)
 
-res = test_create_user()
-assert_that(res.status_code).is_equal_to(201)
+    content = res.json()
+    assert_that(content.get("emailAddress")).is_equal_to("a@b.c")
+    assert_that(content.get("userName")).is_equal_to("TestUser")
+    assert_that(content.get("firstName")).is_equal_to("Test")
+    assert_that(content.get("lastName")).is_equal_to("User")
+    assert_that(content.get("gender")).is_equal_to("F")
+    assert_that(content.get("password")).is_equal_to(None)
+    assert_that(len(content.get("permissions"))).is_equal_to(0)
+
+test_create_user()

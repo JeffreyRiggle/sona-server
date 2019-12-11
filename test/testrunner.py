e38b6dc5cbe5fc4cf69c866c90ede229d8590c88
Jeff <JeffreyRiggle@gmail.com>
2019-12-11 06:46:13 -0500

    better test runner

---
+++ test/testrunner.py
@@ -0,0 +1,31 @@
+import time
+
+allTests = []
+
+def addTest(message, test):
+    global allTests
+    allTests.append({
+        "message": message,
+        "test": test
+    })
+
+def run():
+    global allTests
+    passed = 0
+    failed = 0
+
+    for t in allTests:
+        try:
+            print(t.get("message"))
+            start = time.time()
+            t.get("test")()
+            passed = passed + 1
+            print(t.get("message") + " PASSED in " + str(time.time() - start))
+        except:
+            failed = failed + 1
+            print(t.get("message") + " FAILED")
+    
+    print("COMPLETE " + str(passed) + " out of " + str(len(allTests)) + " PASSED")
+
+    if failed > 0:
+        raise Exception(str(failed) + " Tests failed!")

b5f9e7a07f46c7bbc8d5b0d7f1347a341333c6ab
Jeff <JeffreyRiggle@gmail.com>
2020-03-27 07:48:06 -0400

    copy https into docker runtime

--- src/start.sh
+++ src/start.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
 
 echo $CONFIG > app/start.json
+
+echo "Finding cert"
+jq .securityConfig.cert app/start.json | cut -d "\"" -f 2 > app/temp
+
+if [ -s app/temp ]
+then
+    echo "Found cert"
+    base64 -d app/temp > app/server.cert
+    jq '.securityConfig.cert="app/server.cert"' app/start.json > app/temp
+    echo `cat app/temp` > app/start.json
+    echo `cat app/start.json`
+else
+    echo "No certificate provided"
+fi
+
+echo "Finding key"
+jq .securityConfig.key app/start.json | cut -d "\"" -f 2 > app/temp
+
+if [ -s app/temp ]
+then
+    echo "Found key"
+    base64 -d app/temp > app/server.key
+    jq '.securityConfig.key="app/server.key"' app/start.json > app/temp
+    echo `cat app/temp` > app/start.json
+else
+    echo "No key provided"
+fi
+
+rm app/temp
 app/src app/start.json
